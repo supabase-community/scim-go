50b1b5b033c1a9af91e964839fb2fc8def0ed0ec
mo khan <mo.khan@supabase.io>
2026-09-02 12:24:04 -0600

    refactor: use explicit property names

--- pkg/core/kind.go
+++ pkg/core/kind.go
@@ -9,11 +9,31 @@ type Kind struct {
 }
 
 var (
-	KindGroup                 = Kind{"Group", SchemaGroup, "/Groups"}
-	KindResourceType          = Kind{"ResourceType", SchemaResourceType, "/ResourceTypes"}
-	KindSchema                = Kind{"Schema", SchemaSchema, "/Schemas"}
-	KindServiceProviderConfig = Kind{"ServiceProviderConfig", SchemaServiceProviderConfig, "/ServiceProviderConfig"}
-	KindUser                  = Kind{"User", SchemaUser, "/Users"}
+	KindGroup = Kind{
+		Name:     "Group",
+		Schema:   SchemaGroup,
+		Endpoint: "/Groups",
+	}
+	KindResourceType = Kind{
+		Name:     "ResourceType",
+		Schema:   SchemaResourceType,
+		Endpoint: "/ResourceTypes",
+	}
+	KindSchema = Kind{
+		Name:     "Schema",
+		Schema:   SchemaSchema,
+		Endpoint: "/Schemas",
+	}
+	KindServiceProviderConfig = Kind{
+		Name:     "ServiceProviderConfig",
+		Schema:   SchemaServiceProviderConfig,
+		Endpoint: "/ServiceProviderConfig",
+	}
+	KindUser = Kind{
+		Name:     "User",
+		Schema:   SchemaUser,
+		Endpoint: "/Users",
+	}
 )
 
 func (k Kind) Location(baseURL string) string {
