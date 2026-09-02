b993d8ac61517fe9f205cfd8ba17d65ceda36d32
mo khan <mo.khan@supabase.io>
2026-09-02 15:50:41 -0600

    refactor: remove ExternalID

--- pkg/core/entity.go
+++ pkg/core/entity.go
@@ -3,6 +3,5 @@ package core
 type Entity struct {
 	Schemas []SchemaURI `json:"schemas"`
 	ID      string      `json:"id"`
-	// ExternalID string      `json:"externalId,omitempty"`
-	Meta Meta `json:"meta"`
+	Meta    Meta        `json:"meta"`
 }
