b2445dfabcab016b0107a7e9cabcc25089e1dec3
mo khan <mo.khan@supabase.io>
2026-09-22 12:06:08 -0600

    refactor(protocol)!: pass *protocol.Attribute to Evaluator instead of a key string, extract visitor into visitor.go

--- pkg/protocol/evaluator.go
+++ pkg/protocol/evaluator.go
@@ -1,15 +1,12 @@
 package protocol
 
-import (
-	"github.com/supabase-community/scim-go/pkg/core"
-	"github.com/supabase-community/scim-go/pkg/filter"
-)
+import "github.com/supabase-community/scim-go/pkg/filter"
 
-type Evaluator[T any] interface {
-	Compare(attribute *core.Attribute, key string, op filter.Operator, value any) (T, error)
-	Present(attribute *core.Attribute, key string) (T, error)
-	And(left, right T) (T, error)
-	Or(left, right T) (T, error)
-	Not(operand T) (T, error)
-	ValuePath(attribute *core.Attribute, key string, valueFilter func() (T, error)) (T, error)
+type Evaluator[Output any] interface {
+	Compare(attribute *Attribute, op filter.Operator, value any) (Output, error)
+	Present(attribute *Attribute) (Output, error)
+	And(left, right Output) (Output, error)
+	Or(left, right Output) (Output, error)
+	Not(operand Output) (Output, error)
+	ValuePath(attribute *Attribute, valueFilter func() (Output, error)) (Output, error)
 }
